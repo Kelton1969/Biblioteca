eae9bc762b788b1391c9b57c1e3cae11f79ff3ce
Kelton <kelton.tinho23@gmail.com>
2024-05-09 13:54:48 -0300

    inicio do projeto

---
+++ src/biblioteca/Menu.java
@@ -0,0 +1,43 @@
+package biblioteca;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import biblioteca.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+	int opcao;	
+	Scanner scanner = new Scanner(System.in);
+		System.out.println(Cores.TEXT_RED + Cores.ANSI_WHITE_BACKGROUND);
+		System.out.println("*****************************************************");
+		System.out.println("                                                     ");
+		System.out.println("                Reserve seus livros                  ");
+		System.out.println("                                                     ");
+		System.out.println("=====================================================");
+		System.out.println("                                                     ");
+		System.out.println("            1 - Criar Conta                          ");
+		System.out.println("            2 - Login                                ");
+		System.out.println("            3 - Digite um título                     ");
+		System.out.println("            4 - Atualizar Dados da Conta             ");
+		System.out.println("            5 - Apagar Conta                         ");
+		System.out.println("            6 - Localizar biblioteca                 ");
+		System.out.println("            7 - Loja                                 ");
+		System.out.println("            8 - Doe um livro                         ");
+		System.out.println("            9 - Sair                                 ");
+		System.out.println("                                                     ");
+		System.out.println("=====================================================");
+		System.out.println("Entre com a opção desejada:                          ");
+		System.out.println("                                                     " );
+	
+	
+	try {
+		opcao = scanner.nextInt();
+	} catch (InputMismatchException e) {
+		System.out.println("\nDigite valores inteiros!");
+		scanner.nextLine();
+		opcao = 0;
+	}
+	}
+}
